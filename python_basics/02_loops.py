1c621d2433f44edcea51706f611089f9da004cac
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-02-17 16:24:51 +0530

    added loops

---
+++ python_basics/02_loops.py
@@ -0,0 +1,36 @@
+######## 
+# if 
+# if else
+# if elif
+# if elif else
+
+# age = 19
+# if age > 19:
+#     print("Adult")
+
+# if age > 19:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+# age = 18
+# if age > 18:
+#     print("Adult")
+# elif age == 18:
+#     print("Becoming adult")
+# else:
+#     print("Minor")
+
+# for loop
+# for i in range(0, 5, 2):
+#     print(i)
+
+list_1 = [100, 200, 300, 400]
+for i in range(len(list_1)):
+    print(list_1[i])
+
+list_1 = [100, 200, 300, 400]
+for idx, val in enumerate(list_1):
+    print(idx, val)
+
+# dictonary loop
